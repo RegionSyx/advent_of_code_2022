cddd86e710238467b7c0561c45bf89539b33040c
Eric Hurst <eric.hurst97@gmail.com>
2022-12-06 05:18:13 +0000

    Day 6 Solution

---
+++ day06/solution.py
@@ -0,0 +1,38 @@
+import pytest
+
+@pytest.mark.parametrize('example,result', [
+    ('bvwbjplbgvbhsrlpgdmjqwftvncz', 5),
+    ('nppdvjthqldpwncqszvftbrmjlhg', 6),
+    ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 10),
+    ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 11),
+])
+def test_example1(example, result):
+    output = solution(example, 4)
+    assert output == result
+
+
+@pytest.mark.parametrize('example,result', [
+    ('mjqjpqmgbljsphdztnvjfqwrcgsmlb', 19),
+    ('bvwbjplbgvbhsrlpgdmjqwftvncz', 23),
+    ('nppdvjthqldpwncqszvftbrmjlhg', 23),
+    ('nznrnfrfntjfmvfwmzdfjlvtqnbhcprsg', 29),
+    ('zcfzfwzzqfrljwzlrfnpqdbhtmscgvjw', 26)
+])
+def test_example2(example, result):
+    output = solution(example, 14)
+    assert output == result
+
+
+def solution(signal, marker_length):
+    for i in range(len(signal)):
+        chars = signal[i:i + marker_length]
+
+        if len(set(chars)) == len(chars):
+            return i + marker_length
+
+if __name__ == "__main__":
+    with open("./day06/input.txt") as f:
+        print(solution(f.read(), 4))
+
+    with open("./day06/input.txt") as f:
+        print(solution(f.read(), 14))
